976cf7a336c6492cc4d154059449e9be2e0f7079
Yago Matheus <yagofx@hotmail.com>
2022-12-16 19:57:25 -0300

    First commit

---
+++ src/test/java/br/com/yagofx/gadobot/GadoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.yagofx.gadobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GadoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
