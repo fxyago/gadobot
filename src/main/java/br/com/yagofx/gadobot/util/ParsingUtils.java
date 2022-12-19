4e5de1c92d841439d5969844f7d144a3493761da
Yago Matheus <yagofx@hotmail.com>
2022-12-19 02:26:44 -0300

    Added extra parsing methods

--- src/main/java/br/com/yagofx/gadobot/util/ParsingUtils.java
+++ src/main/java/br/com/yagofx/gadobot/util/ParsingUtils.java
@@ -1,13 +1,28 @@
 package br.com.yagofx.gadobot.util;
 
+import se.michaelthelin.spotify.model_objects.specification.Track;
+import se.michaelthelin.spotify.model_objects.specification.TrackSimplified;
+
 public class ParsingUtils {
 
-    public static String parseCommand(String message) {
+    public static String extractCommandFrom(String message) {
         return message.substring(1).split(" ")[0];
+    }
+
+    public static String extractArgsFrom(String message) {
+        return message.split(" ", 2)[1];
     }
 
     public static String extractSpotifyIdFrom(String url) {
         return url.split("track/|album/|playlist/")[1].split("\\?si")[0];
+    }
+
+    public static String formatSpotifyItem(Track track) {
+        return String.format("%s - %s", track.getArtists()[0], track.getName());
+    }
+
+    public static String formatSpotifyItem(TrackSimplified track) {
+        return String.format("%s - %s", track.getArtists()[0], track.getName());
     }
 
 }
