b6784d8ded32f3154de8f0c8f82666f7d618b4fe
Yago Matheus <yagofx@hotmail.com>
2022-12-19 04:09:57 -0300

    Added a delegate class for the play command

--- src/main/java/br/com/yagofx/gadobot/commands/Play.java
+++ src/main/java/br/com/yagofx/gadobot/commands/Play.java
@@ -1,6 +1,7 @@
 package br.com.yagofx.gadobot.commands;
 
 import br.com.yagofx.gadobot.commands.base.AbstractCommand;
+import br.com.yagofx.gadobot.handlers.DelegatePlayHandler;
 import net.dv8tion.jda.api.events.Event;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import org.springframework.stereotype.Component;
@@ -10,6 +11,12 @@ import java.util.List;
 @Component
 public class Play extends AbstractCommand {
 
+    private final DelegatePlayHandler handler;
+
+    public Play(DelegatePlayHandler handler) {
+        this.handler = handler;
+    }
+
     @Override
     public List<String> getAliases() {
         return List.of("play", "p");
@@ -18,6 +25,7 @@ public class Play extends AbstractCommand {
     @Override
     public Void run(Event event) {
         var messageEvent = (MessageReceivedEvent) event;
+        handler.loadAndPlayFrom(messageEvent);
         messageEvent.getChannel().sendMessage("Playando!!").queue();
         return null;
     }
---
+++ src/main/java/br/com/yagofx/gadobot/handlers/DelegatePlayHandler.java
@@ -0,0 +1,67 @@
+package br.com.yagofx.gadobot.handlers;
+
+import br.com.yagofx.gadobot.player.AudioTrackWrapper;
+import br.com.yagofx.gadobot.service.GuildService;
+import br.com.yagofx.gadobot.service.YoutubeService;
+import br.com.yagofx.gadobot.util.ParsingUtils;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+@Component
+public class DelegatePlayHandler {
+
+    private final GuildService guildService;
+    private final SpotifyHandler spotifyHandler;
+    private final YoutubeService youtubeService;
+
+    public DelegatePlayHandler(
+            @Qualifier("GuildServiceImpl") GuildService guildService,
+            @Qualifier("youtubeServiceProxy") YoutubeService youtubeService,
+            SpotifyHandler spotifyHandler) {
+        this.guildService = guildService;
+        this.spotifyHandler = spotifyHandler;
+        this.youtubeService = youtubeService;
+    }
+
+    public void loadAndPlayFrom(MessageReceivedEvent event) {
+        String args = ParsingUtils.extractArgsFrom(event.getMessage().getContentRaw());
+        final List<AudioTrackWrapper> tracks = new ArrayList<>();
+
+        try {
+            processTrackResults(event, args, tracks);
+            queue(tracks, event.getGuild(), event.getMember());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void queue(List<AudioTrackWrapper> tracks, Guild guild, Member member) {
+        if (!tracks.isEmpty()) {
+            guildService.connectToVoiceChannel(guild, member);
+            guildService.getGuildAudioPlayer(guild).getScheduler().queueAll(tracks);
+        }
+    }
+
+    private void processTrackResults(MessageReceivedEvent event, String args, List<AudioTrackWrapper> tracks) throws ExecutionException, InterruptedException {
+        if (args.contains("spotify")) {
+            tracks.addAll(spotifyHandler.makeWrapperOf(event.getMember(), args));
+        } else if (args.contains("playlist")){
+            youtubeService.getPlaylistFrom(args).forEach(t -> {
+                tracks.add(new AudioTrackWrapper(t, event.getMember(), t.getInfo().title));
+            });
+        } else {
+            AudioTrack audioTrack = youtubeService.getTrackFrom(args);
+            tracks.add(new AudioTrackWrapper(audioTrack, event.getMember(), audioTrack.getInfo().title));
+        }
+    }
+
+}
