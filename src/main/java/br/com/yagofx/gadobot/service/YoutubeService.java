5823bba157f857ced72a9e3ef7e01951f1839f4b
Yago Matheus <yagofx@hotmail.com>
2022-12-19 04:08:54 -0300

    Implemented basic youtube search

--- src/main/java/br/com/yagofx/gadobot/service/YoutubeService.java
+++ src/main/java/br/com/yagofx/gadobot/service/YoutubeService.java
@@ -2,6 +2,10 @@ package br.com.yagofx.gadobot.service;
 
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 
+import java.util.List;
+
 public interface YoutubeService {
     AudioTrack getTrackFrom(String query);
+
+    List<AudioTrack> getPlaylistFrom(String url);
 }
--- src/main/java/br/com/yagofx/gadobot/service/proxy/YoutubeServiceProxy.java
+++ src/main/java/br/com/yagofx/gadobot/service/proxy/YoutubeServiceProxy.java
@@ -6,11 +6,15 @@ import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
 import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.concurrent.ExecutionException;
 
+@Slf4j
 @Service
 public class YoutubeServiceProxy implements YoutubeService {
 
@@ -23,6 +27,11 @@ public class YoutubeServiceProxy implements YoutubeService {
     @Override
     public synchronized AudioTrack getTrackFrom(String query) {
         final AudioTrack[] trackFound = {null};
+
+        if (!query.contains(".com")) query = "ytsearch:" + query;
+
+        log.info(query);
+
         try {
             playerManager.loadItem(query, new AudioLoadResultHandler() {
                 @Override
@@ -50,6 +59,38 @@ public class YoutubeServiceProxy implements YoutubeService {
             throw new NoSuchElementException("An error has ocurred");
         }
         return trackFound[0];
+    }
+
+    @Override
+    public synchronized List<AudioTrack> getPlaylistFrom(String url) {
+        final List<AudioTrack> tracks = new ArrayList<>();
+        try {
+            playerManager.loadItem(url, new AudioLoadResultHandler() {
+                @Override
+                public void trackLoaded(AudioTrack track) {
+                    throw new RuntimeException("Now allowed");
+                }
+
+                @Override
+                public void playlistLoaded(AudioPlaylist playlist) {
+                    tracks.addAll(playlist.getTracks());
+                }
+
+                @Override
+                public void noMatches() {
+                    throw new NoSuchElementException("Nenhuma musica encontrada");
+                }
+
+                @Override
+                public void loadFailed(FriendlyException exception) {
+                    throw exception;
+                }
+            }).get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            throw new RuntimeException("An error has ocurred");
+        }
+        return tracks;
     }
 
 }
