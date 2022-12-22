3de8debebeb24e65b07677661ee292890490d2b4
Yago Matheus <yagofx@hotmail.com>
2022-12-22 20:51:02 -0300

    Implemented Move command

---
+++ src/main/java/br/com/yagofx/gadobot/commands/Move.java
@@ -0,0 +1,46 @@
+package br.com.yagofx.gadobot.commands;
+
+import br.com.yagofx.gadobot.commands.base.AbstractCommand;
+import br.com.yagofx.gadobot.service.GuildService;
+import br.com.yagofx.gadobot.util.ParsingUtils;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
+import net.dv8tion.jda.api.events.Event;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+import java.util.List;
+
+public class Move extends AbstractCommand {
+
+    private final GuildService guildService;
+
+    public Move(GuildService guildService) {
+        this.guildService = guildService;
+    }
+
+    @Override
+    public List<String> getAliases() {
+        return List.of("move", "mv");
+    }
+
+    @Override
+    public void run(Event event) {
+        MessageReceivedEvent messageEvent = (MessageReceivedEvent) event;
+
+        try {
+            String[] args = ParsingUtils.extractArgsFrom(messageEvent.getMessage().getContentRaw()).split(",");
+
+            Integer fromPosition = Integer.parseInt(args[0].trim());
+            Integer toPosition = Integer.parseInt(args[1].trim());
+
+            guildService.getTrackScheduler(messageEvent.getGuild()).move(fromPosition, toPosition);
+            messageEvent.getMessage().addReaction(Emoji.fromUnicode("U+1F44D")).queue();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String helpDescription() {
+        return "* Move musicas de lugar na lista\n* Ex. `;mv 25, 5` move a musica numero 25 da lista para a posicao 5";
+    }
+}
