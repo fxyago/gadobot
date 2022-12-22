e75e268ad6aa430825fdbdd02917942e59da9d26
Yago Matheus <yagofx@hotmail.com>
2022-12-22 19:14:17 -0300

    Moved AbstractCommand methods into Command interface

--- src/main/java/br/com/yagofx/gadobot/commands/base/AbstractCommand.java
+++ src/main/java/br/com/yagofx/gadobot/commands/base/AbstractCommand.java
@@ -1,27 +1,6 @@
 package br.com.yagofx.gadobot.commands.base;
 
 import br.com.yagofx.gadobot.annotation.InheritedComponent;
-import br.com.yagofx.gadobot.listener.CommandListener;
-import jakarta.annotation.PostConstruct;
-import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 
-@Slf4j
 @InheritedComponent
-public abstract class AbstractCommand implements Command {
-
-    @Getter
-    private final String name = this.getClass().getSimpleName();
-    private CommandListener listener;
-
-    @PostConstruct
-    protected final void subscribe() {
-        listener.addCommand(this);
-    }
-
-    @Autowired
-    public void setListener(CommandListener listener) {
-        this.listener = listener;
-    }
-}
+public abstract class AbstractCommand implements Command {}
--- src/main/java/br/com/yagofx/gadobot/commands/base/Command.java
+++ src/main/java/br/com/yagofx/gadobot/commands/base/Command.java
@@ -1,15 +1,28 @@
 package br.com.yagofx.gadobot.commands.base;
 
+import br.com.yagofx.gadobot.listener.CommandListener;
+import jakarta.annotation.PostConstruct;
 import net.dv8tion.jda.api.events.Event;
 
 import java.util.List;
 
 public interface Command {
 
+    default String name() {
+        return this.getClass().getSimpleName();
+    }
+
     default List<String> getAliases() {
         return List.of(this.getClass().getSimpleName().toLowerCase());
     }
 
-    Void run(Event event);
+    void run(Event event);
+
+    String helpDescription();
+
+    @PostConstruct
+    default void subscribe() {
+        CommandListener.addCommand(this);
+    }
 
 }
