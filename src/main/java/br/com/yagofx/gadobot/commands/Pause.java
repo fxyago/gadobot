598e2feea2e61593fd7fbf9a162cbde4952e6edf
Yago Matheus <yagofx@hotmail.com>
2022-12-22 20:50:56 -0300

    Implemented Pause command

--- src/main/java/br/com/yagofx/gadobot/commands/Pause.java
+++ src/main/java/br/com/yagofx/gadobot/commands/Pause.java
@@ -1,12 +1,23 @@
 package br.com.yagofx.gadobot.commands;
 
 import br.com.yagofx.gadobot.commands.base.AbstractCommand;
+import br.com.yagofx.gadobot.service.GuildService;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.Event;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.util.List;
 
 public class Pause extends AbstractCommand {
+
+    private final GuildService guildService;
+
+    public Pause(
+            @Qualifier("GuildServiceImpl")
+            GuildService guildService) {
+        this.guildService = guildService;
+    }
 
     @Override
     public List<String> getAliases() {
@@ -16,11 +27,14 @@ public class Pause extends AbstractCommand {
     @Override
     public void run(Event event) {
         var messageEvent = (MessageReceivedEvent) event;
-        messageEvent.getChannel().sendMessage("Pausando!!").queue();
+        var scheduler = guildService.getTrackScheduler(messageEvent.getGuild());
+        boolean paused = scheduler.isPaused();
+        messageEvent.getMessage().addReaction(Emoji.fromUnicode(paused ? "U+25B6" : "U+23F8")).queue();
+        scheduler.togglePause();
     }
 
     @Override
     public String helpDescription() {
-        return "*Pausa/resume o player de música";
+        return "* Pausa/resume o player de música";
     }
 }
