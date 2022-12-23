04f9147c97381382a93cb9da21db6bf13391008f
Yago Matheus <yagofx@hotmail.com>
2022-12-23 06:58:29 -0300

    Fixed button run returning Void type

--- src/main/java/br/com/yagofx/gadobot/commands/Button.java
+++ src/main/java/br/com/yagofx/gadobot/commands/Button.java
@@ -2,7 +2,6 @@ package br.com.yagofx.gadobot.commands;
 
 import br.com.yagofx.gadobot.commands.base.AbstractCommand;
 import net.dv8tion.jda.api.events.Event;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 import java.util.List;
 
@@ -14,9 +13,10 @@ public class Button extends AbstractCommand {
     }
 
     @Override
-    public Void run(Event event) {
-        MessageReceivedEvent messageEvent = (MessageReceivedEvent) event;
-        messageEvent.getChannel().sendMessage("Muuuu??").queue();
-        return null;
+    public void run(Event event) {}
+
+    @Override
+    public String helpDescription() {
+        return "* Nada no momento...";
     }
 }
