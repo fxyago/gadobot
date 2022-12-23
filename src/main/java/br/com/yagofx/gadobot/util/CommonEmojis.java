9659da7e2cba1f40e579e05f5351979f8be40e35
Yago Matheus <yagofx@hotmail.com>
2022-12-23 06:51:05 -0300

    Created util class for commonly used emojis

---
+++ src/main/java/br/com/yagofx/gadobot/util/CommonEmojis.java
@@ -0,0 +1,16 @@
+package br.com.yagofx.gadobot.util;
+
+import net.dv8tion.jda.api.entities.emoji.Emoji;
+
+public class CommonEmojis {
+
+    public static Emoji THUMBS_UP = Emoji.fromUnicode("U+1F44D");
+    public static Emoji PAUSE = Emoji.fromUnicode("U+23F8");
+    public static Emoji PLAY = Emoji.fromUnicode("U+25B6");
+    public static Emoji STOP = Emoji.fromUnicode("U+23F9");
+    public static Emoji NEXT_TRACK = Emoji.fromUnicode("U+23ED");
+    public static Emoji PREVIOUS_TRACK = Emoji.fromUnicode("U+23EE");
+    public static Emoji SHUFFLE = Emoji.fromUnicode("U+1F500");
+    public static Emoji REPEAT = Emoji.fromUnicode("U+1F501");
+
+}
