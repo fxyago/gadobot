b9356861eb7d3a7d7d4edd342f240873bf204050
Yago Matheus <yagofx@hotmail.com>
2022-12-23 08:04:37 -0300

    PascalCased component names

--- src/main/java/br/com/yagofx/gadobot/commands/Help.java
+++ src/main/java/br/com/yagofx/gadobot/commands/Help.java
@@ -15,7 +15,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-@Component("helpCommand")
+@Component("HelpCommand")
 public class Help extends AbstractCommand {
 
     @Override
