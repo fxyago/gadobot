df29c2ae016d009a02dfbe3c712377a2a39a5f1f
Yago Matheus <yagofx@hotmail.com>
2022-12-23 06:52:58 -0300

    implemented NowPlaying command

--- src/main/java/br/com/yagofx/gadobot/commands/NowPlaying.java
+++ src/main/java/br/com/yagofx/gadobot/commands/NowPlaying.java
@@ -1,11 +1,27 @@
 package br.com.yagofx.gadobot.commands;
 
 import br.com.yagofx.gadobot.commands.base.AbstractCommand;
+import br.com.yagofx.gadobot.player.AudioTrackWrapper;
+import br.com.yagofx.gadobot.service.GuildService;
+import br.com.yagofx.gadobot.util.CommonEmojis;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.Event;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class NowPlaying extends AbstractCommand {
+
+    private final GuildService guildService;
+
+    public NowPlaying(
+            @Qualifier("GuildServiceImpl")
+            GuildService guildService) {
+        this.guildService = guildService;
+    }
 
     @Override
     public List<String> getAliases() {
@@ -14,10 +30,28 @@ public class NowPlaying extends AbstractCommand {
 
     @Override
     public void run(Event event) {
+        MessageReceivedEvent messageEvent = (MessageReceivedEvent) event;
+        AudioTrackWrapper nowPlaying = guildService.getTrackScheduler(messageEvent.getGuild()).getNowPlaying();
+
+        if (nowPlaying == null) {
+            messageEvent.getChannel().sendMessage("Tem nada tocando agora nao bro").queue();
+        } else {
+            messageEvent.getMessage().addReaction(CommonEmojis.THUMBS_UP).queue();
+            messageEvent.getChannel().sendMessageEmbeds(montarEmbedDe(nowPlaying)).queueAfter(2, TimeUnit.SECONDS);
+        }
     }
 
     @Override
     public String helpDescription() {
         return "*Mostra detalhes sobre a música que esta tocando no momento";
     }
+
+    private MessageEmbed montarEmbedDe(AudioTrackWrapper trackWrapper) {
+        EmbedBuilder embedBuilder = new EmbedBuilder()
+                .setAuthor("Tocando agora:")
+                .setTitle(trackWrapper.getSongName(), trackWrapper.getTrack().getInfo().uri)
+                .setDescription("Pedido por: " + trackWrapper.getMember().getAsMention());
+        return embedBuilder.build();
+    }
+
 }
