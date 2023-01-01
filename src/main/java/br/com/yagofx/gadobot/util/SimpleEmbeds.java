c8126e18c5492bd6cdf06d6bccd8684198221c87
Yago Matheus <yagofx@hotmail.com>
2023-01-01 05:55:48 -0300

    Remade SimpleEmbeds methods, expanded songList builder

--- src/main/java/br/com/yagofx/gadobot/util/SimpleEmbeds.java
+++ src/main/java/br/com/yagofx/gadobot/util/SimpleEmbeds.java
@@ -1,21 +1,29 @@
 package br.com.yagofx.gadobot.util;
 
+import br.com.yagofx.gadobot.player.AudioTrackWrapper;
 import net.dv8tion.jda.api.EmbedBuilder;
+
+import java.util.List;
 
 public class SimpleEmbeds {
 
-    private static final EmbedBuilder EMBED_BUILDER = new EmbedBuilder();
-
     public static EmbedBuilder notification(String author, String description) {
-        return EMBED_BUILDER.clear().setAuthor(author).setDescription(description);
+        return new EmbedBuilder().clear().setAuthor(author).setDescription(description);
     }
 
-    public static EmbedBuilder songList(String formattedList, int totalPages, int currentPage) {
-        return EMBED_BUILDER.clear().setTitle("Lista de músicas:").setDescription(formattedList).setFooter(String.format("Página %s/%s", currentPage, totalPages));
+    public static EmbedBuilder songList(List<AudioTrackWrapper> queue, int currentPage, int totalPages) {
+        EmbedBuilder embedBuilder = new EmbedBuilder().setTitle("Lista de músicas:").setFooter(String.format("Página %s/%s", currentPage + 1, totalPages + 1));
+        StringBuilder sb = new StringBuilder();
+
+        int trackIndex = 1 + 10 * currentPage;
+        for (; trackIndex < 10; trackIndex++)
+            sb.append(String.format("%s - %s", trackIndex, queue.get(trackIndex).getSongName()));
+
+        return embedBuilder.setDescription(sb.toString().trim());
     }
 
     public static EmbedBuilder about(String botInfo, String description) {
-        return EMBED_BUILDER.clear().setTitle(botInfo).setDescription(description);
+        return new EmbedBuilder().clear().setTitle(botInfo).setDescription(description);
     }
 
 }
